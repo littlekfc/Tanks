8ca72455b33629d88087a26d761f1052ef1ab59c
EternalWind <eternalwind@live.cn>
2014-07-18 13:57:38 +0800

    Added interfaces for Player, Vehicle and Weapon.

---
+++ Assets/IPlayer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using Vehicles;
+
+namespace Players
+{
+    /// <summary>
+    /// An interface for all players.
+    /// </summary>
+    public interface IPlayer
+    {
+        /// <summary>
+        /// The vehicle this player is controlling.
+        /// </summary>
+        IVehicle Vehicle { get; }
+    }
+}
---
+++ Assets/IVehicle.cs
@@ -0,0 +1,112 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using Weapons;
+
+namespace Vehicles
+{
+    /// <summary>
+    /// An interface for all the vehicles.
+    /// </summary>
+    public interface IVehicle
+    {
+        /// <summary>
+        /// The main weapon.
+        /// </summary>
+        IWeapon MainWeapon { get; }
+
+        /// <summary>
+        /// The secondary weapon. Not all the vehicles have one.
+        /// </summary>
+        IWeapon SecondaryWeapon { get; }
+
+        /// <summary>
+        /// The maximun speed.
+        /// </summary>
+        float MaxSpeed { get; set; }
+
+        /// <summary>
+        /// The current speed.
+        /// </summary>
+        float CurrentSpeed { get; }
+
+        /// <summary>
+        /// The acceleration.
+        /// </summary>
+        float Acceleration { get; set; }
+
+        /// <summary>
+        /// The current orientation of the vehicle.
+        /// </summary>
+        Vector3 VehicleOrientation { get; }
+
+        /// <summary>
+        /// The current orientation of its weapon.
+        /// </summary>
+        Vector3 WeaponOrientation { get; }
+
+        /// <summary>
+        /// The turning speed of the vehicle itself.
+        /// </summary>
+        float VehicleTurningSpeed { get; set; }
+
+        /// <summary>
+        /// The turning speed of the weapon.
+        /// </summary>
+        float WeaponTurningSpeed { get; set; }
+
+        /// <summary>
+        /// The maximun health of the vehicle.
+        /// </summary>
+        float MaxHealth { get; set; }
+
+        /// <summary>
+        /// The current health of the vehicle.
+        /// </summary>
+        float CurrentHealth { get; set; }
+
+        /// <summary>
+        /// Move the vehicle towards a given direction. 
+        /// If the value of the given direction is larger than 0 on the direction of the vehicle's orientation, 
+        /// the vehicle's engine will be on.
+        /// </summary>
+        /// <param name="direction">The direction to move.</param>
+        /// <param name="is_local">Whether the given direction is local or not.</param>
+        void Move(Vector3 direction, bool is_local);
+
+        /// <summary>
+        /// Stop the vehicle.
+        /// </summary>
+        void Stop();
+
+        /// <summary>
+        /// Start pointing the weapon of the vehicle at a given direction.
+        /// </summary>
+        /// <param name="direction">The world target direction.</param>
+        /// /// <param name="is_local">Whether the given direction is local or not.</param>
+        void StartPointWeaponAt(Vector3 direction, bool is_local);
+
+        /// <summary>
+        /// Fire the weapon.
+        /// </summary>
+        /// <param name="is_main_weapon">Whether the weapon being fired is the main weapon or secondary weapon.</param>
+        void Fire(bool is_main_weapon);
+
+        /// <summary>
+        /// Called when the vehicle is hit.
+        /// </summary>
+        /// <param name="damage">The damage this vehicle is taking.</param>
+        void OnHit(float damage);
+
+        /// <summary>
+        /// Called when the vehicle is destroyed.
+        /// </summary>
+        void OnDie();
+
+        /// <summary>
+        /// Called when picking up a weapon.
+        /// </summary>
+        /// <param name="weapon">The weapon being picked up.</param>
+        void OnPickUp(IWeapon weapon);
+    }
+}
