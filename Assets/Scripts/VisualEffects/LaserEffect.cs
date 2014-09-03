8a945e5376edc9ef7f9052c08cd4a5c6264185fc
EternalWind <eternalwind@live.cn>
2014-09-04 00:45:00 +0900

    Refined laser effect.

--- Assets/Scripts/VisualEffects/LaserEffect.cs
+++ Assets/Scripts/VisualEffects/LaserEffect.cs
@@ -1,101 +1,132 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class LaserEffect : MonoBehaviour {
-    public float chargingPeriod = 0.0f;
-    public float dechargingPeriod = 0.0f;
-    public AnimationCurve chargingCurve;
-    public AnimationCurve dechargingCurve;
-    public float maxStrength;
-
-    private LineRenderer laserLine;
-    public float chargingProgress = 0.0f;
-
-    private float ChargingProgress
+namespace VisualEffects
+{
+    public class LaserEffect : MonoBehaviour
     {
-        get
+        public float chargingPeriod = 0.0f;
+        public float dechargingPeriod = 0.0f;
+        public AnimationCurve chargingCurve;
+        public AnimationCurve dechargingCurve;
+        public float maxStrength;
+        public float maxLightIntensity;
+
+        public float PowerLevel
         {
-            return chargingProgress;
+            get
+            {
+                if (isCharging)
+                    return chargingCurve.Evaluate(ChargingProgress);
+                else
+                    return dechargingCurve.Evaluate(ChargingProgress);
+            }
         }
-        set
+
+        private LineRenderer laserLine;
+        private Light light;
+        private float chargingProgress = 0.0f;
+        private bool isCharging = false;
+
+        private float ChargingProgress
         {
-            chargingProgress = Mathf.Clamp01(value);
+            get
+            {
+                return chargingProgress;
+            }
+            set
+            {
+                chargingProgress = Mathf.Clamp01(value);
+            }
         }
-    }
 
-	// Use this for initialization
-	void Awake () {
-        laserLine = GetComponent<LineRenderer>();
-	}
-	
-    void Update()
-    {
-        var offset = new Vector2(-Time.time, 0.0f);
-        laserLine.materials[1].mainTextureOffset = offset;
-    }
-
-    IEnumerator Charge()
-    {
-        if (chargingPeriod <= 0.0f)
-            ChargingProgress = 1.0f;
-
-        while (ChargingProgress < 1.0f)
+        // Use this for initialization
+        void Awake()
         {
-            ChargingProgress += Time.deltaTime / chargingPeriod;
+            laserLine = GetComponent<LineRenderer>();
+            light = GetComponent<Light>();
+        }
 
-            var current_strength = maxStrength * chargingCurve.Evaluate(ChargingProgress);
+        void Update()
+        {
+            var offset = new Vector2(-Time.time, 0.0f);
+            laserLine.materials[1].mainTextureOffset = offset;
+        }
+
+        IEnumerator Charge()
+        {
+            isCharging = true;
+
+            if (chargingPeriod <= 0.0f)
+                ChargingProgress = 1.0f;
+
+            while (ChargingProgress < 1.0f)
+            {
+                ChargingProgress += Time.deltaTime / chargingPeriod;
+
+                SetPowerLevel(PowerLevel);
+
+                yield return null;
+            }
+
+            laserLine.SetWidth(maxStrength, maxStrength);
+
+            yield break;
+        }
+
+        IEnumerator Decharge()
+        {
+            isCharging = false;
+
+            if (dechargingPeriod <= 0.0f)
+                ChargingProgress = 0.0f;
+
+            while (ChargingProgress > 0.0f)
+            {
+                ChargingProgress -= Time.deltaTime / dechargingPeriod;
+
+                SetPowerLevel(PowerLevel);
+
+                yield return null;
+            }
+
+            laserLine.SetWidth(0.0f, 0.0f);
+            gameObject.SetActive(false);
+
+            yield break;
+        }
+
+        void SetPowerLevel(float normalised_power_level)
+        {
+            var current_strength = maxStrength * normalised_power_level;
             laserLine.SetWidth(current_strength, current_strength);
 
-            yield return null;
+            var current_intensity = maxLightIntensity * normalised_power_level;
+            light.intensity = current_intensity;
         }
 
-        laserLine.SetWidth(maxStrength, maxStrength);
-
-        yield break;
-    }
-
-    IEnumerator Decharge()
-    {
-        if (dechargingPeriod <= 0.0f)
-            ChargingProgress = 0.0f;
-
-        while (ChargingProgress > 0.0f)
+        public void SetTargetPosition(Vector3 target)
         {
-            ChargingProgress -= Time.deltaTime / dechargingPeriod;
+            laserLine.SetPosition(1, target);
 
-            var current_strength = maxStrength * dechargingCurve.Evaluate(ChargingProgress);
-            laserLine.SetWidth(current_strength, current_strength);
+            var target_dist = target.sqrMagnitude;
+            var wave_length = target_dist / 100f;
 
-            yield return null;
+            laserLine.materials[1].mainTextureOffset = new Vector2(wave_length, 1.0f);
         }
 
-        laserLine.SetWidth(0.0f, 0.0f);
-        gameObject.SetActive(false);
+        public void StartCharging()
+        {
+            gameObject.SetActive(true);
 
-        yield break;
-    }
+            StopAllCoroutines();
+            StartCoroutine("Charge");
+        }
 
-    public void SetTargetPosition(Vector3 target)
-    {
-        laserLine.SetPosition(1, target);
-
-        var target_dist = target.sqrMagnitude;
-        var wave_length = target_dist / 100f;
-
-        laserLine.materials[1].mainTextureOffset = new Vector2(wave_length, 1.0f);
-    }
-
-    public void StartCharging()
-    {
-        gameObject.SetActive(true);
-
-        StopAllCoroutines();
-        StartCoroutine("Charge");
-    }
-
-    public void StopCharging()
-    {
-        StopAllCoroutines();
-        StartCoroutine("Decharge");
+        public void StopCharging()
+        {
+            StopAllCoroutines();
+            StartCoroutine("Decharge");
+        }
     }
 }
