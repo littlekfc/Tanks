c2f5b1acb01328ab978f70a06f657377c5e0aa09
EternalWind <eternalwind@live.cn>
2014-10-04 14:47:03 +0900

    Used lazy initialization for LaserEffect

--- Assets/Scripts/VisualEffects/LaserEffect.cs
+++ Assets/Scripts/VisualEffects/LaserEffect.cs
@@ -11,45 +11,57 @@ namespace VisualEffects
         public float maxHitEffectScale = 4.0f;
 
         private LineRenderer laserLine;
-        
-        private Transform hitEffect;
-
-        // Use this for initialization
-        void Awake()
+        private LineRenderer LaserLine
         {
-            laserLine = GetComponent<LineRenderer>();
+            get
+            {
+                if (laserLine == null)
+                    laserLine = GetComponent<LineRenderer>();
 
-            hitEffect = transform.FindChild("HitEffect");
+                return laserLine;
+            }
+        }
+
+        private Transform hitEffect;
+        private Transform HitEffect
+        {
+            get
+            {
+                if (hitEffect == null)
+                    hitEffect = transform.FindChild("HitEffect");
+
+                return hitEffect;
+            }
         }
 
         void Update()
         {
             var offset = new Vector2(-Time.time, 0.0f);
-            laserLine.materials[1].mainTextureOffset = offset;
+            LaserLine.materials[1].mainTextureOffset = offset;
         }
 
         public void SetPowerLevel(float normalised_power_level)
         {
             var current_strength = maxStrength * normalised_power_level;
-            laserLine.SetWidth(current_strength, current_strength);
+            LaserLine.SetWidth(current_strength, current_strength);
 
             var current_intensity = maxLightIntensity * normalised_power_level;
             light.intensity = current_intensity;
 
             var current_hit_intensity = maxHitEffectLightIntensity * normalised_power_level;
-            hitEffect.light.intensity = current_hit_intensity;
+            HitEffect.light.intensity = current_hit_intensity;
 
             var current_hit_scale = maxHitEffectScale * normalised_power_level;
-            hitEffect.localScale = new Vector3(current_hit_scale, current_hit_scale, current_hit_scale);
+            HitEffect.localScale = new Vector3(current_hit_scale, current_hit_scale, current_hit_scale);
         }
 
         public void SetLength(float length)
         {
-            laserLine.SetPosition(1, new Vector3(0.0f, 0.0f, length));
-            hitEffect.localPosition = new Vector3(0.0f, 0.0f, length);
+            LaserLine.SetPosition(1, new Vector3(0.0f, 0.0f, length));
+            HitEffect.localPosition = new Vector3(0.0f, 0.0f, length);
 
             var wave_length = length / 100f;
-            laserLine.materials[1].mainTextureScale = new Vector2(wave_length, 1.0f);
+            LaserLine.materials[1].mainTextureScale = new Vector2(wave_length, 1.0f);
         }
     }
 }
