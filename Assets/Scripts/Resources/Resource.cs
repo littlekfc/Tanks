c1341bdfb94995fb9dd2d1671510ff24dfb9c23e
EternalWind <eternalwind@live.cn>
2014-10-26 15:25:43 +0900

    Implemented resource module.

---
+++ Assets/Scripts/Resources/Resource.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Tanks.Resources
+{
+    /// <summary>
+    /// The in-game resource.
+    /// </summary>
+    public class Resource
+    {
+        public int Mineral { get; set; }
+
+        public Resource()
+        {
+            Mineral = 0;
+        }
+
+        public Resource(Resource other)
+        {
+            Mineral = other.Mineral;
+        }
+    }
+}
---
+++ Assets/Scripts/Resources/ResourceManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+using Tanks.Players;
+
+namespace Tanks.Resources
+{
+    /// <summary>
+    /// A class to manage the in-game resource.
+    /// </summary>
+    public class ResourceManager : Singleton<ResourceManager>
+    {
+        private IDictionary<Team.TeamID, Resource> teamResourceMap = new Dictionary<Team.TeamID, Resource>();
+
+        public void Initialize(Resource initial_resource, IEnumerable<Team.TeamID> teams)
+        {
+            teamResourceMap.Clear();
+
+            foreach (var team in teams)
+            {
+                if (teamResourceMap.ContainsKey(team))
+                {
+                    Debug.LogWarning("Trying to initialize the resource twice for a same team!");
+                }
+                else
+                {
+                    teamResourceMap.Add(team, new Resource(initial_resource));
+                }
+            }
+        }
+
+        public Resource ResourceFor(Team.TeamID team_id)
+        {
+            return teamResourceMap[team_id];
+        }
+    }
+}
