2f6eb39eaa84a2b8eccf0c45e20c9bf25b3ae512
Air <xu.jm01@gmail.com>
2014-11-08 09:09:51 +0800

    commit a gun for test

---
+++ Assets/Scripts/Weapons/BulletGun.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Weapons
+{
+    public class BulletGun: Weapon
+    {
+        public GameObject Bullet;
+
+        public GameObject BulletTip;
+
+        public override bool IsOneShot
+        {
+            get { return true; }
+        }
+
+        public override void Fire()
+        {
+            if (IsCooledDown)
+            {
+                GameObject obj = Instantiate(Bullet, BulletTip.transform.position, BulletTip.transform.rotation) as GameObject;
+                obj.GetComponent<UltimateExplosiveBomb>().bulletGun = this;
+                ResetCoolDownTimer();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Weapons/UltimateExplosiveBomb.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using UnityEngine;
+using Attributes;
+
+namespace Weapons
+{
+    /// <summary>
+    /// The universe super invincible ultimate explosive bomb 
+    /// </summary>
+    public class UltimateExplosiveBomb: MonoBehaviour
+    {
+        /// <summary>
+        /// the bullet's flying speed
+        /// </summary>
+        public float flyingSpeed = 100.0f;
+
+        /// <summary>
+        /// the bullet's life time
+        /// </summary>
+        public float lifeTime = 2.0f;
+
+        public GameObject bombEffect;
+
+        public Weapon bulletGun;
+
+        private Object m_bombEffectIns;
+
+        private bool m_hasBomb = false;
+
+        void Start()
+        {
+            rigidbody.velocity = transform.forward * flyingSpeed;
+        }
+
+        protected virtual void Update()
+        {
+            if (lifeTime > 0.0f)
+                lifeTime -= Time.deltaTime;
+            else
+                Bomb();
+        }
+
+        void OnCollisionEnter(Collision collisionInfo)
+        {
+            Bomb();
+            TObject o = collisionInfo.collider.GetComponent<TObject>();
+            if (o is IDestroyable && bulletGun)
+            {
+                 (o as IDestroyable).Hit(bulletGun.StandardDamage);
+            }
+        }
+
+        IEnumerator DestroyEffect()
+        {
+            GetComponent<MeshRenderer>().enabled = false;
+            yield return new WaitForSeconds(1);
+            Destroy(m_bombEffectIns);
+            Destroy(gameObject);
+        }
+
+        void Bomb()
+        {
+            if (!m_hasBomb)
+            {
+                m_bombEffectIns = Instantiate(bombEffect, gameObject.transform.position, gameObject.transform.rotation);
+                StartCoroutine(DestroyEffect());
+                m_hasBomb = true;
+            }
+        }
+    }
+}
